44bc214dcfce27fef44e073b85820bfe958c8bd1
Carson Moore <Carscafmoo@gmail.com>
2015-05-01 15:58:56 -0400

    Solves all three exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   before_save do 
-    self.email = email.downcase
+    self.email.downcase! #! modifies in place
   end 
   validates :name, {presence: true, length: {maximum: 50}} 
   validates :email, {presence: true, 
                       length: {maximum: 255}, 
-                      format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
+                      format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i},
                       uniqueness: {case_sensitive: false}}
   validates :password, length: {minimum: 5} # BECAUSE WE'RE CRAZY
   has_secure_password
