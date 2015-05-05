8793d997825e8e941aa6659c0c627072059e2c2e
Carson Moore <Carscafmoo@gmail.com>
2015-05-05 09:11:04 -0400

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    ## test errors, yo!
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example User",
+                               email: "user@example.com",
+                               password:              "guest",
+                               password_confirmation: "guest" }
+    end
+    assert_template 'users/show'
+    ## test flash, yo!
+  end
+end
