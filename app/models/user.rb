2bb0dcf4fc7be316619d48a084f8178b70eec928
Carson Moore <Carscafmoo@gmail.com>
2015-05-31 09:47:09 -0400

    Add user following and feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User < ActiveRecord::Base
                       uniqueness: {case_sensitive: false}}
   validates :password, length: {minimum: 5}, allow_blank: true # BECAUSE WE'RE CRAZY
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, { class_name: "Relationship", 
+                                    foreign_key: "follower_id",
+                                    dependent: :destroy }
+  has_many :passive_relationships, { class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy }
+
+  has_many :following, { through: :active_relationships, source: :followed }
+  has_many :followers, { through: :passive_relationships, source: :follower }
 
   has_secure_password
 
@@ -70,7 +79,21 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                 WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN(#{following_ids}) OR user_id = :user_id", { user_id: id })
+  end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
   end
 
   private
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,4 +71,33 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow a user" do
+    guest = users(:guest)
+    archer  = users(:archer)
+    assert_not guest.following?(archer)
+    guest.follow(archer)
+    assert guest.following?(archer)
+    assert archer.followers.include?(guest)
+    guest.unfollow(archer)
+    assert_not guest.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:guest)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 end
